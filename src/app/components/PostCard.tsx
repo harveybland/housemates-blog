f1fbcf6e62d63a23136a6ecb5d5cf5afd380ad42
Harvey Bland <harvey.bland@totum.com>
2024-01-20 11:19:05 +0000

    feat: created postcard component

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
 import { PostProps } from "../../../types/types";
 
 export default function PostCard({ id, title, userId, body }: PostProps) {
   return (
-    <div className="border border-b-2 mb-3">
+    <Link
+      href={`/post/${id}`}
+      className="block border border-b-2 mb-3 p-3 rounded"
+    >
       <h1>{title}</h1>
       <p>{body}</p>
-    </div>
+    </Link>
   );
 }
