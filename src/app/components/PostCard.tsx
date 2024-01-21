6ad1b5fceb4bb2611034c07df94375e7e988ae6f
Harvey Bland <harvey.bland@totum.com>
2024-01-21 15:21:44 +0000

    feat: filter posts by author name

--- src/app/components/PostCard.tsx
+++ src/app/components/PostCard.tsx
@@ -14,7 +14,7 @@ export default function PostCard({
   return (
     <Link
       href={`/post/${id}`}
-      className="flex gap-4 flex-col p-3 rounded min-h-[190px] bg-white shadow-[0px_0px_20px_0px_rgba(0,0,0,.1)] "
+      className="flex gap-4 flex-col p-3 rounded min-h-[230px] max-h-[200px] bg-white shadow-[0px_0px_20px_0px_rgba(0,0,0,.1)] "
     >
       <div className="flex items-center gap-1 hover:underline">
         <Image src={avatar} alt="avatar" width={23} height={23} />
@@ -22,7 +22,7 @@ export default function PostCard({
       </div>
       <div>
         <h1 className="font-semibold mb-1">{title}</h1>
-        <p className="text-brand-space">{body}</p>
+        <p className="text-brand-space blog-text">{body}</p>
       </div>
       <div className="flex gap-4 text-brand-space text-sm">
         <p>1 min read</p>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,46 @@ import PostCard from "./components/PostCard";
 
 export default function Home() {
   const [posts, setPosts] = useState<PostProps[]>([]);
+  const [filteredPosts, setFilteredPosts] = useState<PostProps[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isGrid, setIsGrid] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("");
 
   async function getPosts() {
     const response: PostResponseProps[] = await postsData();
     setPosts(response);
-    if (response) {
-      setIsLoading(false);
-    }
+    setFilteredPosts(response); // Initially set filteredPosts to all posts
+    setIsLoading(false);
   }
 
   useEffect(() => {
     getPosts();
   }, []);
 
+  useEffect(() => {
+    // Filter posts based on user.name and search term
+    const filtered = posts.filter((post) => {
+      const userNameMatches = post.user?.name
+        ?.toLowerCase()
+        .includes(searchTerm.toLowerCase());
+      return userNameMatches;
+    });
+
+    setFilteredPosts(filtered);
+  }, [searchTerm, posts]);
+
   return (
     <div className="page-container relative">
       {isLoading && <div className="loadingBar"></div>}
-      <div className="my-4">
+      <div className="flex justify-between items-center my-4">
+        <div>
+          <input
+            type="text"
+            placeholder="Search Author"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
         <div className="flex gap-2">
           <button
             type="button"
@@ -44,11 +65,11 @@ export default function Home() {
         </div>
       </div>
       <div>
-        {posts.length > 0 && (
+        {filteredPosts.length > 0 && (
           <div
             className={`blog-cards animate-pop-in ${!isGrid && "!grid-cols-1"}`}
           >
-            {posts.map((post: PostProps) => (
+            {filteredPosts.map((post: PostProps) => (
               <div key={post.id}>
                 <PostCard
                   id={post.id}
