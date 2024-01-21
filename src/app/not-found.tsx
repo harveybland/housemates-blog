b2aede5da88f51c0e4f18f9fc1de555eeb99d152
Harvey Bland <harvey.bland@totum.com>
2024-01-21 17:52:16 +0000

    feat: created not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+export default function NotFound() {
+  return (
+    <>
+      <section className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-4xl font-bold">404</h1>
+        <h2 className="text-2xl font-bold">Page Not Found</h2>
+        <p className="text-xl font-bold">Sorry, this page does not exist.</p>
+      </section>
+    </>
+  );
+}
